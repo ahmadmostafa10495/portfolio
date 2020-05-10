0eea7622710f91a621ac9c1db722392ce98998e8
Ahmad Mostafa <33914189+ahmadmostafa10495@users.noreply.github.com>
2020-05-11 01:43:53 +0200

    added social media links and tweaked contact component

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -78,66 +78,80 @@ class Contact extends Component {
     render() {
     	const errors = this.validate(this.state.subject, this.state.email, this.state.message);
     	return(
-				<div className="row row-content">
-                    <div className="col-12">
-                        <h3>Contact</h3>
-                    </div>
-                    <div className="col-12 col-md-9">
-                        <Form onSubmit={this.handleSubmit}>
-                            <FormGroup row>
-                                <Label htmlFor="subject" md={2}>Subject</Label>
-                                <Col md={10}>
-                                    <Input type="text" 
-                                           id="subject" 
-                                           name="subject" 
-                                           placeholder="Subject" 
-                                           value={this.state.subject}
-                                           valid={errors.subject === ''}
-                                           invalid={errors.subject !== ''} 
-                                           onChange={this.handleInputChange} 
-                                           onBlur={this.handleBlur('subject')} />
-                                    <FormFeedback>{errors.subject}</FormFeedback>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor="email" md={2}>Email</Label>
-                                <Col md={10}>
-                                    <Input type="email" 
-                                           id="email" 
-                                           name="email" 
-                                           placeholder="Email" 
-                                           value={this.state.email} 
-                                           valid={errors.email === ''}
-                                           invalid={errors.email !== ''}
-                                           onChange={this.handleInputChange} 
-                                           onBlur={this.handleBlur('email')} />
-                                    <FormFeedback>{errors.email}</FormFeedback>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor="message" md={2}>Your Message</Label>
-                                <Col md={10}>
-                                    <Input type="textarea" 
-                                    	   id="message" 
-                                    	   name="message" 
-                                    	   rows="12" 
-                                    	   value={this.state.message} 
-                                    	   onChange={this.handleInputChange}
-                                           valid={errors.message === ''}
-                                           invalid={errors.message !== ''}
-                                           onBlur={this.handleBlur('message')} />
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Col md={{size: 10, offset: 2}}>
-                                    <Button type="submit" color="primary">
-                                    Send Message
-                                    </Button>
-                                </Col>
-                            </FormGroup>
-                        </Form>
-                    </div>
-                </div>
+    			<div>
+					<div className="row row-content">
+	                    <div className="col-12">
+	                        <h3>Contact</h3>
+	                    </div>
+	                    <div className="col-12 col-md-9">
+	                        <Form onSubmit={this.handleSubmit}>
+	                            <FormGroup row>
+	                                <Label htmlFor="subject" md={2}>Subject</Label>
+	                                <Col md={10}>
+	                                    <Input type="text" 
+	                                           id="subject" 
+	                                           name="subject" 
+	                                           placeholder="Subject" 
+	                                           value={this.state.subject}
+	                                           valid={errors.subject === '' && this.state.touched.subject}
+	                                           invalid={errors.subject !== ''} 
+	                                           onChange={this.handleInputChange} 
+	                                           onBlur={this.handleBlur('subject')} />
+	                                    <FormFeedback>{errors.subject}</FormFeedback>
+	                                </Col>
+	                            </FormGroup>
+	                            <FormGroup row>
+	                                <Label htmlFor="email" md={2}>Email</Label>
+	                                <Col md={10}>
+	                                    <Input type="email" 
+	                                           id="email" 
+	                                           name="email" 
+	                                           placeholder="Email" 
+	                                           value={this.state.email} 
+	                                           valid={errors.email === '' && this.state.touched.email}
+	                                           invalid={errors.email !== ''}
+	                                           onChange={this.handleInputChange} 
+	                                           onBlur={this.handleBlur('email')} />
+	                                    <FormFeedback>{errors.email}</FormFeedback>
+	                                </Col>
+	                            </FormGroup>
+	                            <FormGroup row>
+	                                <Label htmlFor="message" md={2}>Your Message</Label>
+	                                <Col md={10}>
+	                                    <Input type="textarea" 
+	                                    	   id="message" 
+	                                    	   name="message" 
+	                                    	   rows="12" 
+	                                    	   value={this.state.message} 
+	                                    	   onChange={this.handleInputChange}
+	                                           valid={errors.message === '' && this.state.touched.message}
+	                                           invalid={errors.message !== ''}
+	                                           onBlur={this.handleBlur('message')} />
+	                                </Col>
+	                            </FormGroup>
+	                            <FormGroup row>
+	                                <Col md={{size: 10, offset: 2}}>
+	                                    <Button type="submit" color="primary">
+	                                    Send Message
+	                                    </Button>
+	                                </Col>
+	                            </FormGroup>
+	                        </Form>
+	                    </div>
+	                </div>
+	                <div className="col-12 align-self-center">
+	                    <div className="text-center">
+	                        <a className="btn btn-social-icon btn-github mx-1" href="https://github.com/ahmadmostafa10495"><i className="fa fa-github"></i></a>
+	                        <a className="btn btn-social-icon btn-facebook mx-1" href="https://www.facebook.com/ahmed.mostafa.3939503"><i className="fa fa-facebook"></i></a>
+	                        <a className="btn btn-social-icon btn-linkedin mx-1" href="https://www.linkedin.com/in/ahmed--moustafa/"><i className="fa fa-linkedin"></i></a>
+	                        <a className="btn btn-social-icon btn-twitter mx-1" href="https://twitter.com/AhMadMostafa7"><i className="fa fa-twitter"></i></a>
+	                        <a className="btn btn-social-icon btn-google mx-1" href="https://www.youtube.com/channel/UCKiXWLA1CvSE8D0cT9d2lSA?"><i className="fa fa-youtube"></i></a>
+	                        <a className="btn btn-social-icon btn-instagram mx-1" href="https://www.instagram.com/ahmd_mostafa/"><i className="fa fa-instagram"></i></a>	          
+	                        <a className="btn btn-social-icon btn-soundcloud mx-1" href="https://soundcloud.com/ahmed-mostafa-1012"><i className="fa fa-soundcloud"></i></a>	          
+	                        <a className="btn btn-social-icon btn-yahoo mx-1" href="mailto:a7mad53@ymail.com"><i className="fa fa-yahoo"></i></a>
+	                    </div>
+	                </div>
+            	</div>
     	);
     }
 }
