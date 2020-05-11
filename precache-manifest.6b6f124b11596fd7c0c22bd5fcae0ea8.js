3ae6c9aa763d4d567459c9a9bf0bde75d6adfa17
Ahmad Mostafa <33914189+ahmadmostafa10495@users.noreply.github.com>
2020-05-11 23:43:37 +0200

    Updates

---
+++ precache-manifest.6b6f124b11596fd7c0c22bd5fcae0ea8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a44689c205e25ab4cce348122cb470",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8c4b7277d6341a3b20f9",
+    "url": "/portfolio/static/css/2.60668f69.chunk.css"
+  },
+  {
+    "revision": "c39300f4487a5f0da5d7",
+    "url": "/portfolio/static/css/main.91ffd736.chunk.css"
+  },
+  {
+    "revision": "8c4b7277d6341a3b20f9",
+    "url": "/portfolio/static/js/2.7075db97.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.7075db97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39300f4487a5f0da5d7",
+    "url": "/portfolio/static/js/main.0eb74951.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
